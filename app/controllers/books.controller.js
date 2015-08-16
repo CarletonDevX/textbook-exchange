65c13d7e59c427e6268f6ad109dea18ded107dec
David Pickart <pickartd@carleton.edu>
2015-08-16 00:40:16 -0400

    Simple API route

--- app/controllers/books.controller.js
+++ app/controllers/books.controller.js
@@ -9,7 +9,6 @@ exports.renderBook = function(req, res, next) {
               res.type('txt').send('No book found by those parameters :(');
             } else {
                 Listing.find({ISBN: book.ISBN}, function(err, listings) {
-                    console.log(listings[0].user.avatar);
                     if (!err) {
                         res.render('book', {
                             'book': book,
@@ -24,4 +23,24 @@ exports.renderBook = function(req, res, next) {
             res.json(err);
         }
     });
-}
+};
+
+exports.getBook = function(req, res, next) {
+    path = req.url;
+    isbn = path.substr(path.lastIndexOf('/') + 1);
+    if (isbn.length < 1) {
+        res.status(400).send('Needs ISBN.');
+    } else {
+        Book.findOne({ISBN: isbn}, function(err, book) {
+            if (!err) {
+                if (!book) {
+                    res.status(404).send('Book not found by those conditions.');
+                } else {
+                    res.json(book);
+                }
+            } else {
+                res.json(err);
+            }
+        });
+    }
+};
