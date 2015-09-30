1ad2d92440847d0dac77b37928879aac8f32f68e
David Pickart <pickartd@carleton.edu>
2015-09-30 11:48:03 -0500

    Prevent duplicate offerings

--- app/controllers/offers.controller.js
+++ app/controllers/offers.controller.js
@@ -13,9 +13,28 @@ exports.countOffers = function(req, res, next) {
     });
 }
 
+exports.getOffersForListing = function (req, res, next) {
+    var listing = req.rListing;
+    Offer.find({listingID: listing._id}, function (err, offers) {
+        if (!err) {
+            req.rOffers = offers;
+            next();
+        } else {
+            Error.mongoError(req, res, err);
+        }
+    });
+}
+
 exports.makeOffer = function (req, res, next) {
     var listing = req.rListing;
     var user = req.rUser;
+    var offers = req.rOffers;
+    for (var i = 0; i < offers.length; i++) {
+        if (offers[i].buyerID == user._id) {
+            Error.errorWithStatus(req, res, 400, "You've already made an offer on this listing.");
+            return;
+        }
+    };
     if (user._id == listing.userID) {
         Error.errorWithStatus(req, res, 400, "You can't make an offer on your own listing.");
         return;
