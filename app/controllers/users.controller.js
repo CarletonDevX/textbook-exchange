45ae9a1b7c9a884881f32b17c9946a54273bd874
David Pickart <pickartd@carleton.edu>
2015-10-04 13:50:52 -0500

    Registration call (and more)

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -8,7 +8,7 @@ var User = require('mongoose').model('users'),
 
 exports.countUsers = function (req, res, next) {
     if (!req.rSchoolStats) req.rSchoolStats = {};
-    User.count({}, function (err, count) {
+    User.count({verified: true}, function (err, count) {
         if (!err) {
             req.rSchoolStats.numUsers = count;
             next();
@@ -18,12 +18,62 @@ exports.countUsers = function (req, res, next) {
     });
 }
 
-exports.getCurrentUser = function(req, res, next) {
+function validateEmail (email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@carleton.edu$/i;
+    // ((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$
+    return re.test(email);
+}
+
+function validatePassword (password) {
+    var re = /^[A-Za-z0-9 ]{5,}$/i;
+    return re.test(password);
+}
+
+exports.createUser = function (req, res, next) {
+    var info = req.body;
+    var email = info.email;
+    var password = info.password;
+    if (!validateEmail(email)) {
+        Error.errorWithStatus(req, res, 400, 'Must provide a valid Carleton email address.');
+        return;
+    }
+    if (!validatePassword(password)) {
+        Error.errorWithStatus(req, res, 400, 'Must provide a valid password (5+ alphanumeric characters).');
+        return;
+    }
+
+    var user = new User({"email": email, "password": password});
+    user.verified = false;
+    user.provider = 'local';
+    var md5 = crypto.createHash('md5');
+    user.verifier = md5.update((Math.random()*100).toString()).digest('hex');
+
+    // Optional details
+    if (info.name) user.name = info.name;
+    if (info.avatar) user.avatar = info.avatar;
+    if (info.bio) user.bio = info.bio;
+    if (info.gradYear) user.gradYear = info.gradYear;
+
+    avatars.getAvatarWithID(null, function (image) {
+        user.avatar = image;
+        user.save(function(err, user) {
+            if (!err) {
+                req.rUser = user;
+                req.rListings = [];
+                next();
+            } else {
+                Error.mongoError(req, res, err);
+            }
+        });
+    });
+}
+
+exports.getCurrentUser = function (req, res, next) {
     req.rUser = req.user;
     next();
 }
 
-exports.updateUser = function(req, res, next) {
+exports.updateUser = function (req, res, next) {
     var user = req.rUser;
     if (req.user._id != user._id) {
         Error.errorWithStatus(req, res, 401, 'Unauthorized to update user.');
@@ -58,7 +108,10 @@ exports.deleteUser = function (req, res, next) {
 }
 
 exports.getUser = function (req, res, next) {
-    var userID = req.params.userID;
+    // Get ID from either params or previous middleware
+    var userID = req.rUserID;
+    if (!userID) userID = req.params.userID;
+
     User.findOne({_id: userID}, function(err, user) {
         if (!err) {
             if (!user) {
@@ -138,80 +191,12 @@ exports.clearUserSubscriptions = function (req, res, next) {
     });
 }
 
-function validateEmail(email) {
-    var re = /^([\w-]+(?:\.[\w-]+)*)@carleton.edu/i;
-    // ((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$
-    return re.test(email);
-}
-
-// Error handling
-var getErrorMessage = function(err) {
-    var message = '';
-    if (err.code) {
-        switch (err.code) {
-            case 11000:
-            case 11001:
-                message = 'An account with that email address already exists.';
-                break;
-            default:
-                message = 'Something went wrong.';
-        }
-    } else {
-        for (var errName in err.errors) {
-            if (err.errors[errName].message) {
-                message = err.errors[errName].message;
-            }
-        }
-    }
-    return message;
-};
-
-// Create a new user and send a verification email
-exports.register = function(req, res, next) {
-    var user = new User(req.body);
-    if (!validateEmail(user.email)) {
-        req.flash('error', 'Please enter a Carleton email address.');
-        return res.redirect('/register');
-    }
-    user.name = {
-        givenName: req.body.givenName,
-        familyName: req.body.familyName,
-        fullName: req.body.givenName + " " + req.body.familyName
-    }
-    user.verified = false;
-    user.provider = 'local';
-    var md5 = crypto.createHash('md5');
-    user.providerId = md5.update((Math.random()*100).toString()).digest('hex');
-    avatars.getAvatarWithID(null, function (image) {
-        user.avatar = image;
-        user.save(function(err) {
-            if (!err) {
-                // Send verification email
-                link = "http://" + req.get('host') + "/verify?email=" + user.email + "&id=" + user.providerId;
-                mailOptions={
-                    to : user.email,
-                    subject : "Please confirm your email account",
-                    html : "Hello,<br> Please click on the link to verify your email.<br><a href=" + link + ">Click here to verify.</a>" 
-                }
-                mailer.sendMail(mailOptions, function(err2, response) {
-                    if(!err2){
-                        req.flash('alert', 'Verification email sent.');
-                        return res.redirect('/');
-                    } else {
-                        req.flash('error', 'Verification email not sent.');
-                        return res.redirect('/');
-                    }
-                });
-            } else {
-                req.flash('error', getErrorMessage(err));
-                return res.redirect('/register');
-            }
-        });
-    });
-};
+/************
+    LEGACY
+*************/
 
 // Verify an account with a given provider ID
-exports.verify = function(req, res, next) {
+exports.verify = function (req, res, next) {
     User.update(
         {email: req.query.email, providerId: req.query.id},
         {verified : true},
@@ -229,7 +214,7 @@ exports.verify = function(req, res, next) {
 };
 
 // Return a user with the profile ID if it exists, else create a new one
-exports.saveOAuthUserProfile = function(req, profile, done) {
+exports.saveOAuthUserProfile = function (req, profile, done) {
     User.findOne(
         {email: profile.email},
         function(err, user) {
@@ -250,46 +235,4 @@ exports.saveOAuthUserProfile = function(req, profile, done) {
                 return done(err);
             }
         });
-};
-
-// Find and list all users *if logged in*
-exports.renderUsers = function(req, res, next) {
-    if (req.user) {
-        User.find({}, 
-            function(err, results) {
-                if (!err) {
-                    res.render('users', {
-                        "Users": results
-                    });
-                } else {
-                    res.json(err);
-                }
-            });
-    } else {
-        res.render('error', {
-            message: "You need to be logged in to see those!"
-        });
-    }
-};
-
-// Display login page
-exports.renderLogin = function(req, res, next) {
-    res.render('login', {
-        errors: req.flash('error'),
-        alerts: req.flash('alert')
-    });
-}
-
-// Display registration page
-exports.renderRegister = function(req, res, next) {
-    res.render('register', {
-        errors: req.flash('error'),
-        alerts: req.flash('alert')
-    });
-}
-
-// Logout
-exports.logout = function(req, res) {
-    req.logout();
-    res.redirect('/');
 };
