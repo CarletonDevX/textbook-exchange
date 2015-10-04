753badcc39f3d0f6cf3bcdcbb5e166294505cbda
JSlote <joseph.slote@gmail.com>
2015-10-03 19:43:21 -0500

    update config.js

---
+++ app/config/config.js
@@ -0,0 +1,20 @@
+var secureConfig = require("./secure-config");
+
+module.exports = function () {
+	switch(process.env.NODE_ENV) {
+		case "development":
+			var settings = secureConfig.settingsWithPort(1337);
+			settings.db = "mongodb://localhost/textbook-exchange-development";
+			settings.mailEnabled = false;
+			return settings;
+		case "test":
+			var settings = secureConfig.settingsWithPort(6969);
+			settings.db = "mongodb://localhost/textbook-exchange-test";
+			settings.mailEnabled = false;
+			return settings;
+		case "production":
+			return null;
+		default:
+			return null;
+	}
+}
