cbcb0a245dc3d38d48760338877d69711dc31660
JSlote <joseph.slote@gmail.com>
2015-10-05 17:57:48 -0500

    search tasties

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -23,6 +23,17 @@ var debounce = function (func, threshold, execAsap) {
   };
 };
 
+//measure width of text, e.g. in input element
+//based on http://stackoverflow.com/a/18109656
+$.fn.textWidth = function(text, font) {
+    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+    var htmlText = text || this.val() || this.text();
+    htmlText = $.fn.textWidth.fakeEl.text(htmlText).html(); //encode to Html
+    htmlText = htmlText.replace(/\s/g, "&nbsp;"); //replace trailing and leading spaces
+    $.fn.textWidth.fakeEl.html(htmlText).css('font', font || this.css('font'));
+    return $.fn.textWidth.fakeEl.width();
+};
+
 hitsTheBooks.directive('ngHtbKeypress', function() {
     return function(scope, element, attrs) {
 
@@ -30,7 +41,7 @@ hitsTheBooks.directive('ngHtbKeypress', function() {
             var keyCode = event.which || event.keyCode;
             var map = scope.$eval("("+attrs.ngHtbKeypress+")");
             if (map[keyCode]){
-	            scope.$eval(map[keyCode])
+              scope.$eval(map[keyCode])
                 event.preventDefault();
             }
         });
@@ -38,138 +49,152 @@ hitsTheBooks.directive('ngHtbKeypress', function() {
 });
 
 hitsTheBooks.run(function($rootScope, $state){
-	$rootScope.is = function(name){ return $state.is(name) };
-	$rootScope.includes = function(name){ return $state.includes(name) };
+  $rootScope.is = function(name){ return $state.is(name) };
+  $rootScope.includes = function(name){ return $state.includes(name) };
 });
 //END UTILS
 
 hitsTheBooks.config(function($stateProvider, $locationProvider) {
-	$stateProvider
+  $stateProvider
 
-		.state('main',{
-			url: '/',
-			templateUrl : '/partials/main',
-			controller  : 'mainController'
-		})
-		.state('main.search',{
-			resolve : {
-				//get results of search from server
-				results: function($http, $stateParams) {
-				  return $http({
-				    method : 'GET',
-				       url : '/api/search?query=' + $stateParams.query
-				  });
-				}
-			},
-			url : 'search?query',
-			sticky: true,
-			views:{
-				'search' : {
-					templateUrl : '/partials/search',
-					controller  : 'searchController'
-				}
-			}
-		})
-		.state('main.detail',{
-			url:'',
-			sticky: true,
-			views: {
-				'detail' : {
-					templateUrl : '/partials/detail'
-				}
-			}
-		})
-		.state('main.detail.book',{
-			resolve : {
-				bookInfo: function($http, $stateParams) {
-					return $http({
-						method : 'GET',
-						url : '/api/book/' + $stateParams.isbn
-					});
-				}
-			},
-			url : 'book/:isbn',
-			templateUrl : '/partials/detail.book',
-			controller  : 'bookController'
-		})
-		.state('main.detail.user',{
-			url : 'user/:userID',
-			resolve : {
-				userInfo: function($http, $stateParams) {
-					return $http({
-						method : 'GET',
-						url : '/api/user/' + $stateParams.userID
-					});
-				}
-			},
-			templateUrl : '/partials/detail.user',
-			controller  : 'userPageController'
-		})
+    .state('main',{
+      url: '/',
+      templateUrl : '/partials/main',
+      controller  : 'mainController'
+    })
+    .state('main.search',{
+      resolve : {
+        //get results of search from server
+        results: function($http, $stateParams) {
+          return $http({
+            method : 'GET',
+               url : '/api/search?query=' + $stateParams.query
+          });
+        }
+      },
+      url : 'search?query',
+      sticky: true,
+      views:{
+        'search' : {
+          templateUrl : '/partials/search',
+          controller  : 'searchController'
+        }
+      }
+    })
+    .state('main.detail',{
+      url:'',
+      sticky: true,
+      views: {
+        'detail' : {
+          templateUrl : '/partials/detail'
+        }
+      }
+    })
+    .state('main.detail.book',{
+      resolve : {
+        bookInfo: function($http, $stateParams) {
+          return $http({
+            method : 'GET',
+            url : '/api/book/' + $stateParams.isbn
+          });
+        }
+      },
+      url : 'book/:isbn',
+      templateUrl : '/partials/detail.book',
+      controller  : 'bookController'
+    })
+    .state('main.detail.user',{
+      url : 'user/:userID',
+      resolve : {
+        userInfo: function($http, $stateParams) {
+          return $http({
+            method : 'GET',
+            url : '/api/user/' + $stateParams.userID
+          });
+        }
+      },
+      templateUrl : '/partials/detail.user',
+      controller  : 'userPageController'
+    })
 
 
-		.state('otherwise', {
-			url: "*path",
-			template: "Oops! We don't know how to serve you (404)"
-		})
+    .state('otherwise', {
+      url: "*path",
+      template: "Oops! We don't know how to serve you (404)"
+    })
 
-		//use HTML5 History API
-		$locationProvider.html5Mode(true);
+    //use HTML5 History API
+    $locationProvider.html5Mode(true);
 })
 
 
 hitsTheBooks.controller('headerController', function($scope, $state, $document) {
-	$scope.closeBlurb = function(){
-		$("#blurb").addClass("hidden");
-	}
+  $scope.closeBlurb = function(){
+    $("#blurb").addClass("hidden");
+  }
 })
 
 hitsTheBooks.controller('mainController', function($scope, $state, $document) {
-	var streamSearchDelay = 200; //ms
-	var initSearch = false;
+  var streamSearchDelay = 200; //ms
+  var initSearch = false;
 
-	//inject the query if we init on the search page
-	if($state.is('main.search')){
-		$scope.searchInput = $state.params.query;
-	}
+  //inject the query if we init on the search page
+  if($state.is('main.search')){
+    $scope.searchInput = $state.params.query;
+  }
 
-	$scope.handleSearchPaneClick = function(){
-		if ($state.includes('main.detail')) {
-			$state.go('main.search',{query: $scope.searchInput});
-		}
-	}
-	
-	//the classic type and hit [enter] search
-	$scope.search = function() {
-		$state.go('main.search',{query:$scope.searchInput})
-	}
-	
-	$scope.resetInitSearch = function(){ initSearch = false }
+  $scope.handleSearchPaneClick = function(){
+    if ($state.includes('main.detail')) {
+      $state.go('main.search',{query: $scope.searchInput});
+    }
+  }
 
-	$scope.initSearch = function(){
-		if (!initSearch && $scope.searchInput) {
-			initSearch = true;
-			$state.go('main.search',{query:$scope.searchInput},{location:'replace'});
-		}
-	}
+  $scope.$on('$stateChangeStart', 
+  function(event, toState, toParams, fromState, fromParams){
+    if (toState.name=="main"){
+      $('input#search-box').val('')
+    }
+  })
 
-	//when typing, perform a throttled search
-	$scope.streamSearch = debounce(function(){
-		if($scope.searchInput) {
-			$state.go('main.search',{query:$scope.searchInput},{location:'replace'});
-		}
-	},streamSearchDelay);
+  $scope.resizeSearchBox = function(){
+    var $this = $('input#search-box');
+    $this.width($this.textWidth()+25);
+  }
+  
+  //the classic type and hit [enter] search
+  $scope.search = function() {
+    if (!initSearch && $scope.searchInput) {
+      $state.go('main.search',{query:$scope.searchInput})
+    }
+  }
+  
+  $scope.resetInitSearch = function(){ initSearch = false }
+
+  $scope.initSearch = function(){
+    if (!initSearch && $scope.searchInput) {
+      initSearch = true;
+      $state.go('main.search',{query:$scope.searchInput},{location:'replace'});
+    }
+  }
+
+  //when typing, perform a throttled search
+  $scope.streamSearch = debounce(function(){
+    if($scope.searchInput) {
+      $state.go('main.search',{query:$scope.searchInput},{location:'replace'});
+    }
+  },streamSearchDelay);
 
 });
 
 hitsTheBooks.controller('searchController', function($scope, results, $stateParams) {
-	$scope.query = $stateParams.query;
-	$scope.results = results.data;
+  $scope.query = $stateParams.query;
+  $scope.results = results.data;
 });
 
 hitsTheBooks.controller('bookController', function($scope, bookInfo, $state, $stateParams) {
-	$scope.book = bookInfo.data;
+  $scope.book = bookInfo.data;
 });
 
 hitsTheBooks.controller('userPageController', function($scope, userInfo, $stateParams) {
-	$scope.user = userInfo.data;
+  $scope.user = userInfo.data;
 });
