fc96fad587826e9ac034c9c67806b7bbc4c89d51
David Pickart <pickartd@carleton.edu>
2015-11-08 14:55:21 -0600

    Check watchlist email settings before sending email

--- app/mailer.js
+++ app/mailer.js
@@ -23,9 +23,9 @@ exports.sendOfferEmail = function (req, res, next) {
 	var offerer = req.rUser;
 
 	options = {
-		to : lister.email,
-		subject : "Someone has made an offer on your book " + book.name,
-		html : "Hello, <br> " + offerer.name.fullName + " has made an offer on your book " + book.name + ". Email them back at " + offerer.email + " to discuss this offer." 
+		subject: "Someone has made an offer on your book " + book.name,
+		html: "Hello, <br> " + offerer.name.fullName + " has made an offer on your book " + book.name + ". Email them back at " + offerer.email + " to discuss this offer.",
+		user: lister
 	}
 
 	sendMail(req, res, next, options);
@@ -37,32 +37,34 @@ exports.sendSubscribersEmail = function (req, res, next) {
 	var subscribers = req.rSubscribers;
 	var recipients = []
 	for (var i = 0; i < subscribers.length; i++) {
-		recipients.push(subscribers[i].email);
+		recipients.push(subscribers[i]);
 	};
 	options = {
-		toMultiple: recipients,
-		subject : "Someone has posted a listing for a book on your watchlist.",
-		html : "Hello, <br> Someone has posted a listing for " + book.name + ". The listing ID is " + listing._id 
+		subject: "Someone has posted a listing for a book on your watchlist.",
+		html: "Hello, <br> Someone has posted a listing for " + book.name + ". The listing ID is " + listing._id,
+		users: recipients,
+		setting: "watchlist" 
 	}
 
 	sendMailToMultipleRecipients(req, res, next, options);
 }
 
 var sendMailToMultipleRecipients = function (req, res, next, options) {
-	var recipients = options.toMultiple;
-	if (recipients.length == 0) {
+	var users = options.users;
+	if (users.length == 0) {
 		next();
 	} else {
 		var sendOptions = options;
-		sendOptions.to = recipients.pop();
-		sendOptions.toMultiple = recipients;
+		sendOptions.user = users.pop();
+		sendOptions.users = users;
 		sendOptions.callback = sendMailToMultipleRecipients
 		sendMail(req, res, next, sendOptions);
 	}
 }
 
 var sendMail = function (req, res, next, options) {
-	if (config.mailEnabled) {
+	if (config.mailEnabled && user.emailSettings[options.setting]) {
+		options.to = user.email;
 	    sender.sendMail(options, function(err) {
 	        if(!err){
 				if (options.callback) {
