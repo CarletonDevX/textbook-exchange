e552744fe0723d62afae5be4eb1958e9f2d5c370
David Pickart <pickartd@carleton.edu>
2016-03-25 00:28:31 -0400

    Require names for users, use first names if unauthorized

--- app/model.js
+++ app/model.js
@@ -53,9 +53,9 @@ mongoose.model('reports', reportSchema, 'reports');
 
 var UserSchema = new Schema({
     name: {
-        givenName: String,
-        familyName: String,
-        fullName: String
+        givenName: { type: String, required: true },
+        familyName: { type: String, required: true },
+        fullName: { type: String, required: true }
     },
     email: { type: String, unique: true, required: true },
     emailSettings: {
