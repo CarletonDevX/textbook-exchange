6abd1c724eb83bdf53fa49a235f8f388309342ec
David Pickart <davidstoreypickart@gmail.com>
2016-07-22 20:08:53 -0400

    Overhaul listings controller

--- app/controllers/listings.controller.js
+++ app/controllers/listings.controller.js
@@ -1,39 +1,32 @@
 var Listing = require('mongoose').model('listings'),
-    Error = require('../errors');
+    HTBError = require('../errors').HTBError,
+    MongoError = require('../errors').MongoError,
+    AmazonError = require('../errors').AmazonError;
 
 exports.countListings = function (req, res, next) {
     if (!req.rSchoolStats) req.rSchoolStats = {};
     Listing.count({completed: false}, function (err, count) {
-        if (!err) {
-            req.rSchoolStats.numListings = count;
-            next();
-        } else {
-            Error.mongoError(req, res, err);
-        }
+        if (err) return next(new MongoError(err));
+        req.rSchoolStats.numListings = count;
+        return next();
     });
 }
 
 exports.getUserListings = function (req, res, next) {
     var userID = req.rUser._id;
-    Listing.find({completed: false, userID: userID}).lean().exec(function(err, listings) {
-        if (!err) {
-            req.rListings = listings;
-            next();
-        } else {
-            Error.mongoError(req, res, err);
-        }
+    Listing.find({completed: false, userID: userID}, function(err, listings) {
+        if (err) return next(new MongoError(err));
+        req.rListings = listings;
+        return next();
     });
 };
 
 exports.getBookListings = function (req, res, next) {
     var ISBN = req.rBook.ISBN;
-    Listing.find({completed: false, ISBN: ISBN}).lean().exec(function(err, listings) {
-        if (!err) {
-            req.rListings = listings;
-            next();
-        } else {
-            Error.mongoError(req, res, err);
-        }
+    Listing.find({completed: false, ISBN: ISBN}, function(err, listings) {
+        if (err) return next(new MongoError(err));
+        req.rListings = listings;
+        return next();
     });
 };
 
@@ -43,155 +36,89 @@ exports.getUndercutListings = function (req, res, next) {
 
     // Get listings with prices higher than the new listing
     var orClause = []
-    if (listing.rentingPrice != null) {
-        orClause.push({"rentingPrice": {$gt: listing.rentingPrice}});
-    }
-    if (listing.sellingPrice != null) {
-        orClause.push({"sellingPrice": {$gt: listing.sellingPrice}});
-    }
-    Listing.find({completed: false, ISBN: ISBN, $or: orClause}).lean().exec(function(err, listings) {
-        if (!err) {
-            req.rUndercutListings = listings;
-            next();
-        } else {
-            Error.mongoError(req, res, err);
-        }
+    if (listing.rentingPrice != null)  orClause.push({"rentingPrice": {$gt: listing.rentingPrice}});
+    if (listing.sellingPrice != null)  orClause.push({"sellingPrice": {$gt: listing.sellingPrice}});
+
+    Listing.find({completed: false, ISBN: ISBN, $or: orClause}, function(err, listings) {
+        if (err) return next(new MongoError(err));
+        req.rUndercutListings = listings;
+        return next();
     });
 };
 
 exports.getListing = function (req, res, next) {
-    // Get ID from either params or previous middleware
     var listingID = req.rListingID || req.params.listingID;
-
     Listing.findOne({_id: listingID}, function(err, listing) {
-        if (!err) {
-            if (!listing) {
-                Error.errorWithStatus(req, res, 404, 'Listing not found by those conditions.');
-            } else {
-                req.rListings = [listing];
-                next();
-            }
-        } else {
-            Error.mongoError(req, res, err);
-        }
+        if (err) return next(new MongoError(err));
+        if (!listing) return next(new HTBError(404, 'Listing not found by those conditions.'));
+        req.rListings = [listing];
+        return next();
     });
 };
 
 exports.createListing = function (req, res, next) {
-    var userListings = req.rListings;
-    for (var i = 0; i < userListings.length; i++) {
-        if (userListings[i].ISBN == req.rBook.ISBN) {
-            Error.errorWithStatus(req, res, 400, 'User already has a listing for this book. ID: ' + userListings[i]._id);
-            return;
-        }
+    var listings = req.rListings;
+    var book = req.rBook;
+    for (var i = 0; i < listings.length; i++) {
+        if (listings[i].ISBN == book.ISBN) return next(new HTBError(400, 'User already has a listing for this book. ID: ' + userListings[i]._id));
     }
-
     var newListing = new Listing(req.body);
-    if (newListing.sellingPrice == null && newListing.rentingPrice == null) {
-        Error.errorWithStatus(req, res, 400, 'Must include "sellingPrice" and/or "rentingPrice" attribute(s).');
-        return;
-    }
-
-    if (newListing.condition == null) {
-        Error.errorWithStatus(req, res, 400, 'Must include "condition" attribute.');
-        return;
-    }
-
+    if (newListing.sellingPrice == null && newListing.rentingPrice == null) return next(new HTBError(400, 'Must include "sellingPrice" and/or "rentingPrice" attribute(s).'));
+    if (newListing.condition == null) return next(new HTBError(400, 'Must include "condition" attribute.'));
     newListing.userID = req.user._id;
-    newListing.ISBN = req.rBook.ISBN;
+    newListing.ISBN = book.ISBN;
     newListing.completed = false;
 
     newListing.save(function(err, listing) {
-        if (!err) {
-            req.rListings = [listing];
-            next();
-        } else {
-            Error.mongoError(req, res, err);
-        }
+        if (err) return next(new MongoError(err));
+        req.rListings = [listing];
+        return next();
     });
 };
 
 exports.updateListing = function (req, res, next) {
     var listing = req.rListings[0];
-    if (req.user._id != listing.userID) {
-        Error.errorWithStatus(req, res, 401, 'Unauthorized to update listing.');
-    } else {
-        var updates = req.body;
+    var updates = req.body;
+    if (req.user._id != listing.userID) return next(new HTBError(401, 'Unauthorized to update listing.'));
 
-        // Only updates to condition, sellingPrice and rentingPrice are allowed
-        if (updates.condition != null) listing.condition = updates.condition;
+    // Only updates to condition, sellingPrice and rentingPrice are allowed
+    if (updates.condition != null) listing.condition = updates.condition;
+    if (updates.sellingPrice != null) listing.sellingPrice = updates.sellingPrice;
+    if (updates.rentingPrice != null) listing.rentingPrice = updates.rentingPrice;
+    // Add null values for prices if value is < 0
+    if (listing.sellingPrice < 0) listing.sellingPrice = null;
+    if (listing.rentingPrice < 0) listing.rentingPrice = null;
+    if (listing.sellingPrice == null && listing.rentingPrice == null) return next(new HTBError(400, 'Listing must have a "sellingPrice" or "rentingPrice".'));
 
-        // Add null values for prices if value is < 0
-        if (updates.sellingPrice != null) {
-            if (updates.sellingPrice < 0) {
-                listing.sellingPrice = null;
-            } else {
-                listing.sellingPrice = updates.sellingPrice;
-            }
-        }
-        if (updates.rentingPrice != null) {
-            if (updates.rentingPrice < 0) {
-                listing.rentingPrice = null;
-            } else {
-                listing.rentingPrice = updates.rentingPrice;
-            }
-        }
-
-        if (listing.sellingPrice == null && listing.rentingPrice == null) {
-            Error.errorWithStatus(req, res, 400, 'Listing must have a "sellingPrice" or "rentingPrice".');
-            return;
-        }
-
-        listing.save(function(err, listing) {
-            if (!err) {
-                req.rListings = [listing];
-                next();
-            } else {
-                Error.mongoError(req, res, err);
-            }
-        });
-    }
+    listing.save(function(err, listing) {
+        if (err) return next(new MongoError(err));
+        req.rListings = [listing];
+        return next();
+    });
 };
 
 exports.removeListings = function (req, res, next) {
     var listings = req.rListings;
     var listingIDs = [];
     for (var i = 0; i < listings.length; i++) {
-        var listing = listings[i];
-        if (req.user._id != listing.userID) {
-            Error.errorWithStatus(req, res, 401, 'Unauthorized to delete listing.');
-            return;
-        }
-        listingIDs.push(listing._id);
+        if (req.user._id != listings[i].userID) return next(new HTBError(401, 'Unauthorized to delete listing.'));
+        listingIDs.push(listings[i]._id);
     };
-    if (listingIDs.length > 0) {
-        Listing.remove({_id: {$in: listingIDs}}, function(err) {
-            if (!err) {
-                next();
-            } else {
-                Error.mongoError(req, res, err);
-            }
-        });
-    } else {
-        next();
-    }
-    
+    if (listingIDs.length == 0) return next();
+    Listing.remove({_id: {$in: listingIDs}}, function(err) {
+        if (err) return next(new MongoError(err));
+        return next();
+    });
 };
 
 exports.completeListing = function (req, res, next) {
     var user = req.rUser;
     var listing = req.rListings[0];
-    if (listing.userID != user._id) {
-        Error.errorWithStatus(req, res, 401, 'Unauthorized to complete listing.');
-    } else {
-        listing.completed = true;
-        listing.save(function(err, listing) {
-            if (!err) {
-                req.rListings = [listing];
-                next();
-            } else {
-                Error.mongoError(req, res, err);
-            }
-        });
-    }
+    if (listing.userID != user._id) return next(new HTBError(401, 'Unauthorized to complete listing.'));
+    listing.completed = true;
+    listing.save(function(err, listing) {
+        if (err) return next(new MongoError(err));
+        req.rListings = [listing];
+        return next();
+    });
 }
