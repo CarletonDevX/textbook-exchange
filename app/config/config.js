a6619f0405453739468b02610faf66ca5d4f295a
David Pickart <davidstoreypickart@gmail.com>
2016-08-11 16:06:12 -0500

    Let heroku assign the port for us

--- app/config/config.js
+++ app/config/config.js
@@ -27,20 +27,20 @@ module.exports = function () {
     };
     switch (env.NODE_ENV) {
     case 'development':
-        settings.port = 1337;
+        settings.port = env.PORT || 1337;
         settings.db = 'mongodb://localhost/textbook-exchange-development';
         settings.mailEnabled = true;
         settings.url = 'localhost:' + settings.port;
         settings.subdomain_offset = 1;
         break;
     case 'test':
-        settings.port = 6969;
+        settings.port = env.PORT || 1337;
         settings.db = 'mongodb://localhost/textbook-exchange-test';
         settings.mailEnabled = false;
         settings.url = 'localhost:' + settings.port;
         break;
     case 'production':
-        settings.port = 1337;
+        settings.port = env.PORT || 1337;
         settings.db = env.MONGODB_URI;
         settings.mailEnabled = true;
         settings.url = 'hitsthebooks.com';
